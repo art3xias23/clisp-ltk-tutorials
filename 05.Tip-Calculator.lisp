f6bda5b518bc2ea2fd68657fca6ab7f84f792d7c
Tino <Konstantin.v.milchev@gmail.com>
2023-10-19 23:37:06 +0100

    Adding tip calculator changes

--- 05.Tip-Calculator.lisp
+++ 05.Tip-Calculator.lisp
@@ -4,51 +4,38 @@
 
 (use-package :ltk)
 
-(defun hello ()
+(defun run ()
     (with-ltk ()
       (wm-title *tk* "Tip Calculator")
-      (let ((content (make-instance 'frame)))
-        (let* ((a-rdb (make-instance 'radio-button
-                                     :master content
-                                     :text "Cheap"
-                                     :variable "tip"
-                                     :value "5")) 
-               (b-rdb (make-instance 'radio-button
-                                                                  :master content
-                                                                  :text "Medium"
-                                                                  :variable "tip"
-                                                                  :value "15"))
-               (c-rdb (make-instance 'radio-button
-                                                                   :master content
-                                                                   :text "Generous"
-                                                                   :variable "tip"
-                                                                   :value "25"))
-               (total (make-instance 'entry
-                                                    :master content
-                                                    :text "Total Fee"))
+      (let* ((a-rdb (make-instance 'radio-button
+                                   :text "Cheap"
+                                   :variable "tip"
+                                   :value "5")) 
+             (b-rdb (make-instance 'radio-button
+                                                                :text "Medium"
+                                                                :variable "tip"
+                                                                :value "15"))
+             (c-rdb (make-instance 'radio-button
+                                                                 :text "Generous"
+                                                                 :variable "tip"
+                                                                 :value "25"))
+             (total (make-instance 'entry
+                                                  :text ""))
 
-               (total-tip (make-instance 'label
-                                                       :master content
-                                                       :text "")))
-              (flet ((calculate ()
-                                (let ((amount   (* (if(numberp (text total)) (text-total) 0) (/(value a-rdb) 100))))
-                                   (setf (text total-tip) amount))))
-                  (grid (make-instance 'label
-                                       :master content
-                                       :text "Total Tip") 3 1)
-                  (grid total-tip 3 3)
-                
-                  (grid total 1 3)
-                  (grid a-rdb 2 1)
-                  (grid b-rdb 2 2)
-                  (grid c-rdb 2 3)
-                  (grid (make-instance 'button
-                                            :master content
-                                            :text "Calculate"
-                                            :command #'calculate))
-                  (grid (make-instance 'label
-                                                :master content
-                                                :text "Total Tip") 3 1)
-                  (grid total-tip 3 3))))))
+             (total-tip (make-instance 'label
+                                                     :text "")))
+            (flet ((calculate ()
+                              (let ((amount   (* (if(numberp (text total)) (text-total) 0) (/ (parse-integer (value c-rdb)) 100))))
+                                 (setf (text total-tip) amount))))
+                (grid total 1 3)
+                (grid a-rdb 2 1)
+                (grid b-rdb 2 2)
+                (grid c-rdb 2 3)
+                (grid (make-instance 'button
+                                          :text "Calculate"
+                                          :command (lambda() (calculate) 3 2)))
+                (grid (make-instance 'label
+                                              :text "Total Tip") 3 1)
+                (grid total-tip 3 3)))))
 
-(hello)
+(run)
