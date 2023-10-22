810b55e58f7dddeb8d41401afb30840bd314e366
Tino <Konstantin.v.milchev@gmail.com>
2023-10-22 23:49:28 +0100

    Added hard and soft search UI elements and padded them appropriately

--- 15.Listbox.lisp
+++ 15.Listbox.lisp
@@ -30,17 +30,48 @@
    (wm-title *tk* "Example listbox")
    (let* (
           (frame (make-instance 'frame))
+          (selection-frame (make-instance 'frame
+                                          :master frame))
+          (hard-frame (make-instance 'frame
+                                     :master frame))
+          (dynamic-frame (make-instance 'frame
+                                        :master frame))
           (countries-listbox (make-instance 'scrolled-listbox 
                                             :master frame))
           (country-seleciton (make-instance 'label
                                             :text ""
-                                            :master frame)))
+                                            :master selection-frame))
+          (hard-search-label (make-instance 'label
+                                            :text "Hard Search"
+                                            :master hard-frame))
+          (dynamic-search-label (make-instance 'label
+                                               :text "Dynamic Search"
+                                               :master dynamic-frame))
+          (hard-search-button (make-instance 'button
+                                             :master hard-frame
+                                             :command (lambda() (hard-search))))
+          (dynamic-search-button (make-instance 'button
+                                                :master dynamic-frame
+                                                :command (lambda() (dynamic-search))))
+          (hard-search-entry (make-instance 'entry
+                                            :master hard-frame))
+          (dynamic-search-entry (make-instance 'entry
+                                               :master dynamic-frame)))
      (grid frame 0 0)
+     (grid selection-frame 0 1 :rowspan 3 :padx 10 :pady 10)
+     (grid hard-frame 0 2 :rowspan 3 :padx 10 :pady 10)
+     (grid dynamic-frame 0 3 :rowspan 3 :padx 10 :pady 10)
      (grid countries-listbox 0 0 :rowspan 5)
      (grid (make-instance 'label
                           :text "Country selected: "
-                          :master frame)0 1)
-     (grid country-seleciton 1 1)
+                          :master frame)  0  1)
+     (grid country-seleciton  1  1)
+     (grid hard-search-label  0  2)
+     (grid dynamic-search-label  0  3)
+     (grid hard-search-entry  1  2 :pady 10)
+     (grid dynamic-search-entry  1  3 :pady 10)
+     (grid hard-search-button  2  2)
+     (grid dynamic-search-button  2  3)
      (listbox-append countries-listbox *country-names*)
 
      (bind (listbox countries-listbox) "<Double-1>" (lambda(evt) (setf (text country-seleciton)  (nth (first (listbox-get-selection countries-listbox)) *country-names*))))
