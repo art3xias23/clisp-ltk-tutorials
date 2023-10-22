db65d3e8aace8ea8ab7f742b8a3a591d08896a99
Tino <Konstantin.v.milchev@gmail.com>
2023-10-22 19:35:27 +0100

    Printing selection name in console

--- 15.Listbox.lisp
+++ 15.Listbox.lisp
@@ -17,9 +17,9 @@
     (let ((countries-listbox (make-instance 'scrolled-listbox)))
       (grid countries-listbox 0 0 :rowspan 5)
 
-      (listbox-append countries-listbox "Bulgaria")
+      (listbox-append countries-listbox *country-names*)
 
-      (bind (listbox countries-listbox) "<Double-1>" (lambda(evt) (format t "Selection is ~a~%" (listbox-get-selection countries-listbox)))))))
+      (bind (listbox countries-listbox) "<Double-1>" (lambda(evt) (format t "Selection is ~a~%" (nth (first (listbox-get-selection countries-listbox)) *country-names*)))))))
 
 (func)
 
