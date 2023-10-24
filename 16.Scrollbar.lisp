9c0c096685a8c410411046159ff4fe71e9880416
Tino <Konstantin.v.milchev@gmail.com>
2023-10-24 08:00:04 +0100

    Adding communication between listbox and scrollbar

--- 16.Scrollbar.lisp
+++ 16.Scrollbar.lisp
@@ -14,7 +14,14 @@
           (status (make-instance 'label :text "Status message here")))
       (grid listbox 0 0 :sticky "nwes")
       (grid scrollbar 0 1 :sticky "ns")
-      (grid status 1 0 :columnspan 2 :sticky "ew"))))
+      (grid status 1 0 :columnspan 2 :sticky "ew")
+      (configure scrollbar
+                :command (format NIL "~a yview" (widget-path listbox)))
+      (configure listbox
+               :yscrollcommand (format NIL "~a set" (widget-path scrollbar)))
+      (dotimes (i 100)
+        (listbox-append listbox (format NIL "Line ~a of 100" i))))))
+
 (func)
 
 
